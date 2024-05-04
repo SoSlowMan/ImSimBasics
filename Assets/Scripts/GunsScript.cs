e9a6720878728ff9b7678fa69aa77b8c4e6dadd5
SoSlowMan <kosman.forgames@yandex.ru>
2024-05-04 15:48:32 +0300

    Pixelization
    Also gun foundation

---
+++ Assets/Scripts/GunsScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GunsScript : MonoBehaviour
+{
+    //Stats
+    [SerializeField] int damage;
+    [SerializeField] bool allowButtonHold;
+    [SerializeField] int magazineSize, bulletsPerTap;
+    [SerializeField] float timeBetweenShots, timeBetweenShoting, spread, reloadTime, range;
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading; //enum??
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
