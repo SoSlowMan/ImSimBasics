107380a165113125522cd276202196f225dcaf5d
SoSlowMan <kosman.forgames@yandex.ru>
2024-09-22 14:27:16 +0300

    a bunch of trash

--- Assets/Scripts/Misc/Objects.cs
+++ Assets/Scripts/Misc/Objects.cs
@@ -1,11 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class Objects : HealthScript
 {
-
-    // Start is called before the first frame update
     void Start()
     {
         health = 10;
